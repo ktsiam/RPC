d595051f1889d1234a6c08c54066b5e9e9a815c8
ktsiam01 <Konstantinos.Tsiampouris@tufts.edu>
2019-11-02 14:08:54 -0400

    jic we fuck up

--- simplefunction.proxy.cpp
+++ simplefunction.proxy.cpp
@@ -1,17 +1,20 @@
 #include "rpcproxyhelper.h"
 #include <tuple>
+#include <iostream>
 
 template <class Ret_T, class ...Args>
 Ret_T generic(const char *fname, Args ...vargs) {
+    std::cerr << "STARTING GENERIC\n";
     RPCPROXYSOCKET->write(fname, strlen(fname)+1); 
-
+    std::cerr << "WRITING FILENAME: " << fname << std::endl;
     std::tuple<Args...> args{vargs...};    
     RPCPROXYSOCKET->write((char*)&args, sizeof(args));
-
+    
+    std::cerr << "READING RESULT\n";
     char readBuffer[sizeof(Ret_T)];
     RPCPROXYSOCKET->read(readBuffer, sizeof(Ret_T));
     Ret_T ret = *reinterpret_cast<Ret_T*>(readBuffer);
-    
+    std::cerr << "RETURNING : " << ret << std::endl;
     return ret;
 }
 
